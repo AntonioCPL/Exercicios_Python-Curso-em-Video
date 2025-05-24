e3dcb2e075755d37c4ab3e7070feea94a5e107a8
Antonio César Paz Loyola <loyolaacpl@gmail.com>
2025-05-24 11:22:03 -0300

    style: Adiciona um 'de' no exercício '\CalculadordeHipotenusa.py'.

--- CalculadordeHipotenusa.py
+++ CalculadordeHipotenusa.py
@@ -3,4 +3,4 @@ print('{:-^40}' .format(t))
 
 co = float(input('Digite o valor do cateto oposto: '))
 ca = float(input("Agora digite o valor do cateto adjacente: "))
-print('Sabendo que a soma dos catetos ao quadrado é igual a hipotenusa ao quadrado,\no valor da hipotenusa é {:.2f}' .format((co**2+ca**2)**(1/2)))
+print('Sabendo que a soma dos catetos ao quadrado é igual a hipotenusa ao quadrado,\no valor da hipotenusa é de {:.2f}' .format((co**2+ca**2)**(1/2)))
